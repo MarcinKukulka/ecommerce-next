696d8ad2ae48e69d7a17a25dd61f5f174ca02102
Marcin <marcinkukulka@gmail.com>
2023-09-09 13:54:21 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<div className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl ">
 			<h1>Eccommerce</h1>
-			<Link href="/products" />
+			<Link href="/products" >Products</Link>
 		</div>
 	);
 }
