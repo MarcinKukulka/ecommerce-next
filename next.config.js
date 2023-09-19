690ad541e2e18113eb1b7699744e42c59e620006
Marcin <marcinkukulka@gmail.com>
2023-09-19 20:00:34 +0200

    added redirects

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
 		typedRoutes: true,
 		mdxRs: true,
 	},
+	redirects: async () => {
+		return [
+			{
+				source: "/products",
+				destination: "/products/1",
+				permanent: true,
+			},
+		];
+	},
 	images: {
 		remotePatterns: [
 			{
