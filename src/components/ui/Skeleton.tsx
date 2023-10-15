9fe2c9583375c90bedf27c4ca804ed9a08d0483d
Marcin <marcinkukulka@gmail.com>
2023-10-15 21:16:35 +0200

    bugfix

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -8,6 +8,7 @@ export const SkeletonLoading = () => {
 				<svg
 					className="h-10 w-10 text-gray-200 dark:text-gray-600"
 					aria-hidden="true"
+					aria-busy="true"
 					xmlns="http://www.w3.org/2000/svg"
 					fill="currentColor"
 					viewBox="0 0 20 18"
