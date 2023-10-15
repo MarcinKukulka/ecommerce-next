5acd0c954e74e9e6739f16eb8d0b46127969d727
Marcin <marcinkukulka@gmail.com>
2023-10-15 20:33:32 +0200

    bugfix

--- src/app/products/[page]/page.tsx
+++ src/app/products/[page]/page.tsx
@@ -1,15 +1,8 @@
-import { getProductsByPage, getProductsList } from "@/api/products";
+import { getProductsByPage } from "@/api/products";
 import { ProductList } from "@/components/ProductList";
 import { Pagination } from "@/components/ui/Pagination";
 import { Sort } from "@/components/ui/Sort";
 import { type ProductOrderByInput } from "@/gql/graphql";
-
-export async function generateStaticParams() {
-	const products = await getProductsList();
-	const pageNumber = Math.ceil(products.length / 20);
-	const pages = Array.from({ length: pageNumber }, (_, i) => i + 1);
-	return pages.map((page) => ({ params: { page: page.toString() } }));
-}
 
 export default async function Products({
 	params,
